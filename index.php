9181e09c44fe7c15800829a73fc65a8045e8d1b2
Ananegrini <anap.carat@gmail.com>
2022-08-19 21:45:44 -0300

    aula19-08-22

---
+++ index.php
@@ -0,0 +1,37 @@
+<?php
+//comentário php linha
+/* comenta tudo
+
+echo  " meu nome Ana Paula";
+$num = 10;
+$nome = "Maria";
+$Altura = 1.75;
+$solteiro = true;
+const idade = 10;
+echo idade;
+if (10>12)
+{
+    echo "falso";
+}
+else
+{
+    echo "10 não é maior que 12";
+}
+// faça um if que calcule a media e escreva na tela se foi aprovado ou não
+*/
+$nota1 = 5;
+$nota2 = 6;
+$res = ($nota1 + $nota2) / 2;
+
+if($res >=6)
+{
+    echo "Aprovado";
+}
+else{
+    echo "Reprovado";
+}
+// concatenção no php é com . ex (echo "f".$nome."a";)
+// do while testa no final 
+// while testa no começo 
+
+?>
